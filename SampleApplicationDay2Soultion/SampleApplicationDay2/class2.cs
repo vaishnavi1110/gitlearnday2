9fb5849195931f0e606e020ba4ba3229fc85566b
vaishnavi gawande <Vaishnavi.Gawande@lntinfotech.com>
2020-12-16 10:53:50 +0530

    change 3

---
+++ SampleApplicationDay2Soultion/SampleApplicationDay2/class2.cs
@@ -0,0 +1,11 @@
+using System;
+namespace SampleApplicationDay2
+{
+    partial class Program
+    {
+        void printWelcome()
+        {
+            Console.WriteLine("Hello user");
+        }
+    }
+}
