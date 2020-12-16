921c15cfaf8528386daa8b71c113d8937baff1ff
vaishnavi gawande <Vaishnavi.Gawande@lntinfotech.com>
2020-12-16 09:29:36 +0530

    CS project Update

--- SampleApplicationDay2Soultion/SampleApplicationDay2/Program.cs
+++ SampleApplicationDay2Soultion/SampleApplicationDay2/Program.cs
@@ -7,6 +7,8 @@ namespace SampleApplicationDay2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("new line");
             Console.ReadKey();
         }
     }
