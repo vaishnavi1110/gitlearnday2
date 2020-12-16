3b3f85cf1375991e9b10ec042abcca4cc96b3699
vaishnavi gawande <Vaishnavi.Gawande@lntinfotech.com>
2020-12-16 10:44:11 +0530

    Change1

---
+++ SampleApplicationDay2Soultion/SampleApplicationDay2/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApplicationDay2
+{
+    partial class Class1
+    {   void printmsg() {
+            Console.WriteLine("welcome to class1 ");
+        }
+        
+    }
+}
